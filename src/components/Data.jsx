64b62ad7001bff585b12832ebdb6728bb1534fa6
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-08 18:02:05 -0500

    feat(Data): template created

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -5,7 +5,35 @@ import '@styles/components/Data.scss'
 const Data = () => {
   return (
     <div className='data'>
-      Data
+      <div className='data__item'>
+        <h3 className='data__item-title'>IP ADDRESS</h3>
+
+        <p className='data__item-content'>192.212.174.101</p>
+      </div>
+
+      <span />
+
+      <div className='data__item'>
+        <h3 className='data__item-title'>LOCATION</h3>
+
+        <p className='data__item-content'>Bogotá, CO</p>
+      </div>
+
+      <span />
+
+      <div className='data__item'>
+        <h3 className='data__item-title'>TIMEZONE</h3>
+
+        <p className='data__item-content'>UTC -05:00</p>
+      </div>
+
+      <span />
+
+      <div className='data__item'>
+        <h3 className='data__item-title'>ISP</h3>
+
+        <p className='data__item-content'>Google</p>
+      </div>
     </div>
   )
 }
