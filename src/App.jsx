f9c9dde44324cd5920cc1f9ce41c71e2e0821802
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-08 10:48:47 -0500

    feat(SearchBar, Data): components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
 import '@styles/App.scss'
+import Data from '@components/Data'
+import SearchBar from '@components/SearchBar'
 
 const App = () => {
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <section className='content'>
+        <div className='content__title'>
+          Title
+        </div>
+
+        <SearchBar />
+
+        <Data />
+      </section>
+
+      <section className='map'>
+        Map
+      </section>
+    </>
   )
 }
 
