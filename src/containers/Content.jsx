5aed651d63f4a6169391fc0aeda0c4879608f185
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-08 20:43:41 -0500

    feat(Data): rendering data passed from Content

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Data from '@components/Data'
 import SearchBar from '@components/SearchBar'
 import '@styles/containers/Content.scss'
 
+const initialData = {
+  ip: '8.8.8.8',
+  location: {
+    country: 'US',
+    region: 'California',
+    city: 'Mountain View',
+    lat: 37.38605,
+    lng: -122.08385,
+    postalCode: '94035',
+    timezone: '-07:00',
+    geonameId: 5375480
+  },
+  domains: [
+    '0--9.ru',
+    '000.lyxhwy.xyz',
+    '000180.top',
+    '00049ok.com',
+    '001998.com.he2.aqb.so'
+  ],
+  isp: 'Google LLC',
+  proxy: {
+    proxy: false,
+    vpn: false,
+    tor: false
+  }
+}
+
 const Content = () => {
+  const [data, setData] = useState(initialData)
+
   return (
     <section className='content'>
-      <div className='content__title'>
-        IP Address Tracker
-      </div>
+      <div className='content__title'>IP Address Tracker</div>
 
       <SearchBar />
 
-      <Data />
+      <Data
+        ip={data.ip}
+        city={data.location.city}
+        country={data.location.country}
+        postalCode={data.location.postalCode}
+        timezone={data.location.timezone}
+        isp={data.isp}
+      />
     </section>
   )
 }
