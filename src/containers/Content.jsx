1fc7249a92caab9c0496761b6b27f4017af451c7
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-10 17:01:22 -0500

    feat(Content): container cleared

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,51 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import Data from '@components/Data'
 import SearchBar from '@components/SearchBar'
 import useGeolocation from '@hooks/useGeolocation'
 import '@styles/containers/Content.scss'
 
-// const initialData = {
-//   ip: '8.8.8.8',
-//   location: {
-//     country: 'US',
-//     region: 'California',
-//     city: 'Mountain View',
-//     lat: 37.38605,
-//     lng: -122.08385,
-//     postalCode: '94035',
-//     timezone: '-07:00',
-//     geonameId: 5375480
-//   },
-//   domains: [
-//     '0--9.ru',
-//     '000.lyxhwy.xyz',
-//     '000180.top',
-//     '00049ok.com',
-//     '001998.com.he2.aqb.so'
-//   ],
-//   isp: 'Google LLC',
-//   proxy: {
-//     proxy: false,
-//     vpn: false,
-//     tor: false
-//   }
-// }
-
 const Content = () => {
-  // const [data, setData] = useState()
-
-  // useEffect(() => {
-  //   const KEY = process.env.IPIFY_KEY
-  //   const ip = ''
-  //   const URL = `https://geo.ipify.org/api/v1?apiKey=${KEY}&ipAddress=${ip}`
-
-  //   fetch(URL)
-  //     .then(res => res.json())
-  //     // .then(res => console.log(res))
-  //     .then(res => setData(res))
-  // }, [])
-
   const data = useGeolocation()
 
   return (
