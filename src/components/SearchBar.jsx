d727bb049fe89505661d97d42bed01c3bdcb5292
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-10 17:32:18 -0500

    feat(SearchBar): inner functionality implemented

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import Search from '@img/icon-arrow.svg'
+import Icon from '@img/icon-arrow.svg'
 import '@styles/components/SearchBar.scss'
 
 const SearchBar = () => {
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    return console.log(search)
+  }
+
+  const inputChange = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className='search-bar'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           placeholder='Search for any IP address'
+          onChange={inputChange}
+          value={search}
         />
 
         <button type='submit'>
-          <img src={Search} alt='search icon' />
+          <img src={Icon} alt='search icon' />
         </button>
       </form>
     </div>
