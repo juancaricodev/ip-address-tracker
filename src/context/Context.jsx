09565a6ab63c5cdf2b91cb5c9f0d5d9b59bb1afc
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-12 23:26:58 -0500

    chore(Context): file cleared

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,20 +1,13 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useState } from 'react'
 
-import { getIp } from '@services/ipify'
 import { useGeolocation } from '@hooks/useGeolocation'
 
 export const Context = createContext({})
 
 export const ContextProvider = ({ children }) => {
-  // const [geolocation, setGeolocation] = useState()
   const [ip, setIp] = useState('')
 
   const geolocation = useGeolocation(ip)
-
-  // useEffect(() => {
-  //   getIp(ip)
-  //     .then(res => setGeolocation(res))
-  // }, [ip])
 
   const handleIp = (ip) => {
     setIp(ip)
