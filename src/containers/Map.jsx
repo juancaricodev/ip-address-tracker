d2adaccae093fc3919162dee3b72da036c564344
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-13 17:07:07 -0500

    feat(Map): styles updated

--- src/containers/Map.jsx
+++ src/containers/Map.jsx
@@ -1,30 +1,27 @@
 import React from 'react'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 
 import '@styles/containers/Map.scss'
 import 'leaflet/dist/leaflet.css'
 
 const Map = () => {
   const position = [4.60971, -74.08175]
-  const zoom = 15
+  const zoom = 13
 
   return (
     <div className='map'>
-      <MapContainer
-        center={position}
-        zoom={zoom}
-        scrollWheelZoom={false}
-        zoomControl={false}
-        style={{ width: '100%' }}
-      >
-        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-        <Marker position={position}>
-          {/* <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup> */}
-        </Marker>
-      </MapContainer>
+      {position
+        ? (
+          <MapContainer
+            center={position}
+            zoom={zoom}
+            zoomControl={false}
+          >
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={position} />
+          </MapContainer>)
+        : '...loading'}
     </div>
   )
 }
