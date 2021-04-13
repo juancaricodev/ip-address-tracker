c32fcec7d745f5cf30bf0f60079c0ca13a1eff14
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-12 23:22:39 -0500

    refactor(useGeolocation): ipify logic refactored into hook from Context

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,17 +1,20 @@
 import React, { createContext, useEffect, useState } from 'react'
 
 import { getIp } from '@services/ipify'
+import { useGeolocation } from '@hooks/useGeolocation'
 
 export const Context = createContext({})
 
 export const ContextProvider = ({ children }) => {
-  const [geolocation, setGeolocation] = useState()
+  // const [geolocation, setGeolocation] = useState()
   const [ip, setIp] = useState('')
 
-  useEffect(() => {
-    getIp(ip)
-      .then(res => setGeolocation(res))
-  }, [ip])
+  const geolocation = useGeolocation(ip)
+
+  // useEffect(() => {
+  //   getIp(ip)
+  //     .then(res => setGeolocation(res))
+  // }, [ip])
 
   const handleIp = (ip) => {
     setIp(ip)
--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -1,24 +1,14 @@
 import { useEffect, useState } from 'react'
-import { useSearch } from '@hooks/useSearch'
 
-export const useGeolocation = () => {
-  const [data, setData] = useState()
-  const [ip, setIp] = useState('')
+import { getIp } from '@services/ipify'
 
-  const value = useSearch()
-
-  const KEY = process.env.IPIFY_KEY
-  const URL = `https://geo.ipify.org/api/v1?apiKey=${KEY}&ipAddress=${ip}`
+export const useGeolocation = (ip) => {
+  const [geolocation, setGeolocation] = useState()
 
   useEffect(() => {
-    setIp(value)
-  }, [value])
+    getIp(ip)
+      .then(res => setGeolocation(res))
+  }, [ip])
 
-  useEffect(() => {
-    fetch(URL)
-      .then(res => res.json())
-      .then(res => setData(res))
-  }, [])
-
-  return data
+  return geolocation
 }
