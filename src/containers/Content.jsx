6e5128a602198322c9c696b211e616ea8870c6b4
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-12 22:35:12 -0500

    feat(Context): context now fetching data and rendering in Content

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Data from '@components/Data'
 import SearchBar from '@components/SearchBar'
-import { useGeolocation } from '@hooks/useGeolocation'
+// import { useGeolocation } from '@hooks/useGeolocation'
+import { Context } from '@context/Context'
 import '@styles/containers/Content.scss'
 
 const Content = () => {
-  const data = useGeolocation()
+  // const data = useGeolocation()
+
+  const { geolocation } = useContext(Context)
+  const data = geolocation
 
   return (
     <section className='content'>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,13 +1,23 @@
-import React, { createContext } from 'react'
-import { useGeolocation } from '@hooks/useGeolocation'
+import React, { createContext, useEffect, useState } from 'react'
+// import { useGeolocation } from '@hooks/useGeolocation'
 
 export const Context = createContext({})
 
 export const ContextProvider = ({ children }) => {
-  useGeolocation()
+  const [geolocation, setGeolocation] = useState()
+
+  const ip = ''
+  const KEY = process.env.IPIFY_KEY
+  const URL = `https://geo.ipify.org/api/v1?apiKey=${KEY}&ipAddress=${ip}`
+
+  useEffect(() => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(res => setGeolocation(res))
+  }, [])
 
   return (
-    <Context.Provider value={{ useGeolocation }}>
+    <Context.Provider value={{ geolocation }}>
       {children}
     </Context.Provider>
   )
