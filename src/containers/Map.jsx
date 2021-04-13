497d0d5f90fdc8c01771919059bcc81b239f652c
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-13 16:45:02 -0500

    feat(Map): leaflet implemented

--- src/containers/Map.jsx
+++ src/containers/Map.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+import '@styles/containers/Map.scss'
+import 'leaflet/dist/leaflet.css'
+
 const Map = () => {
+  const position = [4.60971, -74.08175]
+  const zoom = 15
+
   return (
-    <div>
-      I'm a map!
+    <div className='map'>
+      <MapContainer
+        center={position}
+        zoom={zoom}
+        scrollWheelZoom={false}
+        zoomControl={false}
+        style={{ width: '100%' }}
+      >
+        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        <Marker position={position}>
+          {/* <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup> */}
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
