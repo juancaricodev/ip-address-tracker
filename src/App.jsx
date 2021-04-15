654e48377878877687f00ac633ce2a23513d3c30
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-14 21:22:35 -0500

    feat(App.jsx): Helmet implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { Helmet } from 'react-helmet'
 
 import '@styles/App.scss'
 import Content from '@containers/Content'
 import Map from '@containers/Map'
+import { Context } from '@context/Context'
 
 const App = () => {
+  const { ip } = useContext(Context)
+
   return (
-    <main>
-      <Content />
-      <Map />
-    </main>
+    <>
+      <Helmet>
+        <title>
+          {ip.length > 0 ? `${ip} | IP Address Tracker` : 'IP Address Tracker'}
+        </title>
+
+        <meta name='description' content='Search for the location of any IP among with another key information.' />
+      </Helmet>
+
+      <main>
+        <Content />
+        <Map />
+      </main>
+    </>
   )
 }
 
