e06710297b75e4c648bb3119035afec7d22e6c79
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-15 12:20:00 -0500

    feat(inputValid): domainValid implemented

--- src/utils/inputValid.js
+++ src/utils/inputValid.js
@@ -1,6 +1,13 @@
-export const inputValid = (input) => {
+export const ipValid = (input) => {
   const regex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
   const validIp = regex.test(input)
 
   return validIp
+}
+
+export const domainValid = (input) => {
+  const regex = /^[\w-][\w\-.]+(\.)\w{2,5}$/
+  const validDomain = regex.test(input)
+
+  return validDomain
 }
