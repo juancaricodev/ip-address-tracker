bfb2d5e5d1c5dd5b2c0df641f7e963f075c059c6
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-14 22:55:23 -0500

    feat(App.jsx): lazy loading implemented for Map comtainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react'
+import React, { lazy, Suspense, useContext } from 'react'
 
 import { Helmet } from 'react-helmet'
 
 import '@styles/App.scss'
 import Content from '@containers/Content'
-import Map from '@containers/Map'
 import { Context } from '@context/Context'
+
+const Map = lazy(() => import('@containers/Map'))
 
 const App = () => {
   const { ip } = useContext(Context)
@@ -22,7 +23,10 @@ const App = () => {
 
       <main>
         <Content />
-        <Map />
+
+        <Suspense fallback={<h3>Loading...</h3>}>
+          <Map />
+        </Suspense>
       </main>
     </>
   )
