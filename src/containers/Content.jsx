3afed12b86f2744de99cb557f7a5107f3cea83de
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-15 08:56:49 -0500

    feat(Content): lazy loading removed

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,10 +1,10 @@
-import React, { lazy, Suspense, useContext } from 'react'
+import React, { useContext } from 'react'
 
 import { Context } from '@context/Context'
 import '@styles/containers/Content.scss'
 
-const Data = lazy(() => import('@components/Data'))
-const SearchBar = lazy(() => import('@components/SearchBar'))
+import Data from '@components/Data'
+import SearchBar from '@components/SearchBar'
 
 const Content = () => {
   const { geolocation } = useContext(Context)
@@ -13,20 +13,18 @@ const Content = () => {
     <section className='content' aria-label='content section'>
       <h1 className='content__title' aria-label='title'>IP Address Tracker</h1>
 
-      <Suspense fallback={<h3>Loading...</h3>}>
-        <SearchBar />
+      <SearchBar />
 
-        {geolocation
-          ? <Data
-              ip={geolocation.ip}
-              city={geolocation.location.city}
-              country={geolocation.location.country}
-              postalCode={geolocation.location.postalCode}
-              timezone={geolocation.location.timezone}
-              isp={geolocation.isp}
-            />
-          : '...loading'}
-      </Suspense>
+      {geolocation
+        ? <Data
+            ip={geolocation.ip}
+            city={geolocation.location.city}
+            country={geolocation.location.country}
+            postalCode={geolocation.location.postalCode}
+            timezone={geolocation.location.timezone}
+            isp={geolocation.isp}
+          />
+        : '...loading'}
     </section>
   )
 }
